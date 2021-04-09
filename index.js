8b41139c63c3f14852e4fe7cb3c359f6c61e6882
mehedi hasan <mehedihasanibnc@gmail.com>
2021-04-09 18:32:47 +0600

    abcd

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.connect(err => {
 
   app.get('/products', (req, res) => {
     const search = req.query.body
-    productCollection.find({ $name: { $regex: search } })
+    productCollection.find({ $name: { $search: search } })
     .toArray((err, documents) => {
       res.send(documents)
     })
