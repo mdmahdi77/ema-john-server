b2c6206ba74f7aacf0accc15c11df630615a2506
mehedi hasan <mehedihasanibnc@gmail.com>
2021-04-09 18:08:54 +0600

    server

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ client.connect(err => {
   })
 
   app.get('/products', (req, res) => {
-    const search = req.query.body
-    productCollection.find({ name: { $regex: search } })
+    // const search = req.query.body
+    productCollection.find({})
     .toArray((err, documents) => {
       res.send(documents)
     })
