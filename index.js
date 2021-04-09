49e9ede24e6f76275dfe2f7ea5503ca2b4e56606
mehedi hasan <mehedihasanibnc@gmail.com>
2021-04-09 17:36:30 +0600

    adding search

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ client.connect(err => {
   })
 
   app.get('/products', (req, res) => {
-    productCollection.find({})
+    const search = req.query.body
+    productCollection.find({ name: { $regex: search } })
     .toArray((err, documents) => {
       res.send(documents)
     })
